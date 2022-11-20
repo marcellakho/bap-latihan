8f06fcac4462d5bd2bccdaee4b7d1b1e96440af2
marcellakho <marcellakholib02@gmail.com>
2022-11-20 22:24:37 +0700

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,69 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+
+     
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/category" class="nav-link">
+        <i class="fas fa-archive"></i>
+        <p>
+          Category
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+    <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/user-role" class="nav-link">
+          <i class="fas fa-chalkboard"></i>
+          <p>
+            User Role
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/reservation-status" class="nav-link">
+          <i class="fas fa-dolly"></i>
+          
+          <p>
+            Reservation Status
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/missing-item-status" class="nav-link">
+          <i class="fas fa-binoculars"></i>
+          <p>
+            Missing Item Status
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/schedule" class="nav-link">
+          <i class="fas fa-check"></i>
+          <p>
+            Schedule
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/reservation" class="nav-link">
+          <i class="fas fa-inbox"></i>
+          <p>
+            Reservation
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/attendance" class="nav-link">
+          <i class="fas fa-door-closed"></i>
+          <p>
+            Attendance
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
